464a9ec02af6c1a560e4c4fad08e72a83b75bf91
Jay Asbury <vbjay.net@gmail.com>
2020-10-03 14:26:34 -0400

    Setup config endpoint to get config

---
+++ APIIntro/Controllers/Utility.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+using System.Linq;
+
+namespace APIIntro.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class Utility : Controller
+    {
+        private IConfiguration _config;
+
+        public Utility(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // GET: api/Config
+        [HttpGet("Config", Name = nameof(GetConfig))]
+        [Authorize("LocalHostOnly")]
+        public IActionResult GetConfig() => Ok(_config.AsEnumerable());
+    }
+}
--- APIIntro/Startup.cs
+++ APIIntro/Startup.cs
@@ -43,6 +43,9 @@ namespace APIIntro
             });
 
             services.AddControllers();
+
+            services.AddSingleton<IConfiguration>(Configuration);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
