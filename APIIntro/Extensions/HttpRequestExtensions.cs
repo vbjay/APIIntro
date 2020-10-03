cb22e160188974def77ebe9ae82615521c63005e
Jay Asbury <vbjay.net@gmail.com>
2020-10-03 14:26:01 -0400

    Setup localhost only authorization policy
    Prevents access to anything with this policy except by localhost

---
+++ APIIntro/Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+
+using System.Net;
+
+namespace APIIntro.Extensions
+{
+    public static class HttpRequestExtensions
+    {
+        public static bool IsLocal(this HttpRequest req)
+        {
+            var connection = req.HttpContext.Connection;
+            if (connection.RemoteIpAddress != null)
+            {
+                return IPAddress.IsLoopback(connection.RemoteIpAddress);
+            }
+            return connection.RemoteIpAddress == null && connection.LocalIpAddress == null;
+        }
+    }
+}
--- APIIntro/Startup.cs
+++ APIIntro/Startup.cs
@@ -1,16 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using APIIntro.Extensions;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace APIIntro
 {
@@ -26,6 +22,26 @@ namespace APIIntro
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("LocalHostOnly",
+                                        policy => policy.RequireAssertion(context =>
+                                        {
+                                            if (context.Resource is AuthorizationFilterContext mvcContext)
+                                            {
+                                                var cn = mvcContext.HttpContext.Connection;
+                                                bool isLocal = mvcContext.HttpContext.Request.IsLocal();
+                                                //Log.Logger.Information($"Remote:{cn.RemoteIpAddress} Local:{cn.RemoteIpAddress} isLocal:{isLocal}");
+
+                                                return isLocal;
+                                            }
+                                            else
+                                            {
+                                                return true;
+                                            }
+                                        }).Build());
+            });
+
             services.AddControllers();
         }
 
